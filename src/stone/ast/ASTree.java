d6d26c383b19a47584e7adb2b3d1124f8a9a45cc
Leozki <zheqiliao@gmail.com>
2017-07-14 13:25:35 +0800

    ASTree

--- src/stone/ast/ASTree.java
+++ src/stone/ast/ASTree.java
@@ -3,8 +3,16 @@ package stone.ast;
 /**
  * Created by master on 2017/7/13.
  */
-public class ASTree {
-    public String location() {
-        return "";
+public abstract class ASTree implements Iterable<ASTree> {
+    public abstract ASTree child(int i);
+
+    public abstract int numChildren(); // 子节点的个数
+
+    public abstract Iterable<ASTree> children();
+
+    public abstract String location();
+
+    public Iterable<ASTree> iterable() {
+        return children();
     }
 }
